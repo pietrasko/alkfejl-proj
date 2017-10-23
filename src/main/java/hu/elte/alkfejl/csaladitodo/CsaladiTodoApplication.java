ee8e78a66be893e2f7acd8907cece2e03db3e4a5
pietrasko <pietrasko@gmail.com>
2017-10-23 17:39:03 +0200

    Spring Boot init.

---
+++ src/main/java/hu/elte/alkfejl/csaladitodo/CsaladiTodoApplication.java
@@ -0,0 +1,12 @@
+package hu.elte.alkfejl.csaladitodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsaladiTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsaladiTodoApplication.class, args);
+	}
+}
---
+++ src/test/java/hu/elte/alkfejl/csaladitodo/CsaladiTodoApplicationTests.java
@@ -0,0 +1,16 @@
+package hu.elte.alkfejl.csaladitodo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CsaladiTodoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
