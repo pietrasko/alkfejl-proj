30a05f010ac3edb4b9f5426e4d00f7d74a427185
Lali576 <lali576@hotmail.com>
2017-10-24 19:31:02 +0200

    Added controller modul

---
+++ src/main/java/hu/elte/alkfejl/csaladitodo/controller/UserController.java
@@ -0,0 +1,45 @@
+package hu.elte.alkfejl.csaladitodo.controller;
+
+import hu.elte.alkfejl.csaladitodo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+    
+    @GetMapping("")
+    public ResponseEntity<User> user() {
+        if (userService.isLoggedIn()) {
+            return ResponseEntity.ok(userService.getLoggedInUser());
+        }
+        return ResponseEntity.badRequest().build();
+    }
+    
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.register(user);
+    }
+    
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User user) {
+        try {
+            return ResponseEntity.ok(userService.login(user));
+        } catch (UserNotValidException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+    
+    @PostMapping("/logout")
+    public ResponseEntity<User> logout(@RequestBody User user) {
+        userService.logout();
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/hu/elte/alkfejl/csaladitodo/controller/UserNotValidException.java
@@ -0,0 +1,5 @@
+package hu.elte.alkfejl.csaladitodo.controller;
+
+public class UserNotValidException extends Exception {
+    
+}
